cf77b4387d0986c1114058e95acab224641a9e4b
Shivayogi <shivayogihugar47@gmail.com>
2026-09-26 19:27:43 +0530

    chore: ensure all changes are pushed

--- backend/test_chat.js
+++ backend/test_chat.js
@@ -3,10 +3,11 @@ const ragService = require("./services/ragService");
 async function run() {
   try {
     const res = await ragService.generateChatResponse(
-      "hello can you explain this document to me?",
-      [{ id: 1, text: "This is a test document." }],
+      "explain me this document in kannada",
+      [{ id: 1, text: "This is a Non-Disclosure Agreement." }],
+      "English"
     );
-    console.log(res);
+    console.log("Result:", res);
   } catch (err) {
     console.error("Error:", err);
   }
